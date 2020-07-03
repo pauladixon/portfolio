c78bb95faf1ebf207e1bc4eb47f2c9c47d3c1d0d
pauladixon <paulapdixon@gmail.com>
2020-07-02 20:31:03 -0700

    removed dead code

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ let startAngle = 0
 let arc = Math.PI / num
 let ctx
 let outerRadius = 225
-let numRadius = 200
 let innerRadius = 0
 
 let canvas = document.getElementById("canvas")
@@ -20,13 +19,10 @@ function drawSphere() {
             ctx.fillStyle = "#c7a984"
         } else ctx.fillStyle = "#aaaaaa"
         ctx.beginPath()
-        ctx.arc(250, 250, outerRadius, angle, angle + arc, false)
-        ctx.arc(250, 250, innerRadius, angle + arc, angle, true)
+        ctx.arc(outerRadius, outerRadius, outerRadius, angle, angle + arc, false)
+        ctx.arc(outerRadius, outerRadius, innerRadius, angle + arc, angle, true)
         ctx.fill()
         ctx.save()
-        ctx.translate(250 + Math.cos(angle + arc / 2) * numRadius,
-            250 + Math.sin(angle + arc / 2) * numRadius)
-        ctx.rotate(angle + arc / 2 + Math.PI / 2)
         ctx.restore()
     }
     canvas.style.animation = "wheelSpin 32s linear infinite"
